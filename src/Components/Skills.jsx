50bef9b3de0da98b5e5ff0ad43dfca205698a059
elizbaryananush <elizbaryananush27@gmail.com>
2023-12-12 14:42:54 +0400

    initialcommit

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,68 +7,69 @@ import image5 from '../assets/git.png'
 import image6 from '../assets/inchvoraya.png'
 import image7 from '../assets/mongodb.png'
 import image8 from '../assets/node.png'
+import { Tilt } from 'react-tilt'
 
 function Skills() {
     return (
         <div className='Skills' id='Skills'>
             <h1>My skills</h1>
             <div className="cards">
-                <div className="card">
+                <Tilt className="card" options={{ max: 50, scale: 1 }}>
                     <div className="background2">
                         <div className="item2"></div>
                         <div className="item2"></div>
                     </div>
                     <img src={image1} alt="" />
-                </div>
-                <div className="card">
+                </Tilt>
+                <Tilt className="card"  options={{ max: 50, scale: 1 }}>
                     <div className="background2">
                         <div className="item2"></div>
                         <div className="item2"></div>
                     </div>
                     <img src={image2} alt="" />
-                </div>
-                <div className="card">
+                </Tilt>
+                <Tilt className="card"  options={{ max: 50, scale: 1 }}>
                     <div className="background2">
                         <div className="item2"></div>
                         <div className="item2"></div>
                     </div>
                     <img src={image3} alt="" />
-                </div>
-                <div className="card">
+                </Tilt>
+                <Tilt className="card"  options={{ max: 50, scale: 1 }}>
                     <div className="background2">
                         <div className="item2"></div>
                         <div className="item2"></div>
                     </div>
                     <img src={image4} alt="" />
-                </div>
-                <div className="card">
+                </Tilt>
+                <Tilt className="card"  options={{ max: 50, scale: 1 }}>
                     <div className="background2">
                         <div className="item2"></div>
                         <div className="item2"></div>
                     </div>
                     <img src={image5} alt="" />
-                </div>
-                <div className="card">
+                </Tilt>
+                <Tilt className="card"  options={{ max: 50, scale: 1 }}>
                     <div className="background2">
                         <div className="item2"></div>
                         <div className="item2"></div>
                     </div>
                     <img src={image6} alt="" />
-                </div>
-                <div className="card">
+                </Tilt>
+                <Tilt className="card"  options={{ max: 50, scale: 1 }}>
                     <div className="background2">
                         <div className="item2"></div>
                         <div className="item2"></div>
                     </div>
                     <img src={image7} alt="" />
-                </div>
-                <div className="card">
+                </Tilt>
+                <Tilt className="card"  options={{ max: 50, scale: 1 }}>
                     <div className="background2">
                         <div className="item2"></div>
                         <div className="item2"></div>
                     </div>
                     <img src={image8} alt="" />
-                </div>
+                </Tilt>
             </div>
         </div>
     )
