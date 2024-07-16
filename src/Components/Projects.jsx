1d8d78a56258e50db3edac82d1970d3e2a56d789
elizbaryananush <elizbaryananush27@gmail.com>
2024-07-16 18:40:10 +0400

    initialcommit

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,9 +1,10 @@
-import React, { useState , useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import yourbank from '../assets/yourBank.png'
 import plantshop from '../assets/plantshop.png'
 import movie from '../assets/movie.png'
 import info from '../assets/info.png'
 import calc from '../assets/calc.png'
+import taskager from '../assets/project6.png'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { useRef } from 'react'
@@ -38,56 +39,56 @@ function Projects() {
 
     useEffect(() => {
 
-            gsap.from(headingRef.current, {
-                translateX: '-1000px',
-                duration: 1.1,
-                opacity: 0,
-                scrollTrigger: {
-                    trigger: headingRef.current,
-                    start: 'top-=700px',
-                    end: 'bottom center',
-                    toggleActions: 'play none none none',
-                },
-            })
+        gsap.from(headingRef.current, {
+            translateX: '-1000px',
+            duration: 1.1,
+            opacity: 0,
+            scrollTrigger: {
+                trigger: headingRef.current,
+                start: 'top-=700px',
+                end: 'bottom center',
+                toggleActions: 'play none none none',
+            },
+        })
 
-            gsap.from(projectLeftRef1.current, {
-                translateX: '-1000px',
-                duration: 1.1,
-                opacity: 0,
-                ease: 'power4.out',
-                scrollTrigger: {
-                    trigger: projectLeftRef1.current,
-                    start: 'top-=700px',
-                    end: 'bottom center',
-                    toggleActions: 'play none none none',
-                },
-            })
+        gsap.from(projectLeftRef1.current, {
+            translateX: '-1000px',
+            duration: 1.1,
+            opacity: 0,
+            ease: 'power4.out',
+            scrollTrigger: {
+                trigger: projectLeftRef1.current,
+                start: 'top-=700px',
+                end: 'bottom center',
+                toggleActions: 'play none none none',
+            },
+        })
 
-            gsap.from(projectLeftRef2.current, {
-                translateX: '-1000px',
-                duration: 1.1,
-                opacity: 0,
-                ease: 'power4.out',
-                scrollTrigger: {
-                    trigger: projectLeftRef2.current,
-                    start: 'top-=700px',
-                    end: 'bottom center',
-                    toggleActions: 'play none none none',
-                },
-            })
+        gsap.from(projectLeftRef2.current, {
+            translateX: '-1000px',
+            duration: 1.1,
+            opacity: 0,
+            ease: 'power4.out',
+            scrollTrigger: {
+                trigger: projectLeftRef2.current,
+                start: 'top-=700px',
+                end: 'bottom center',
+                toggleActions: 'play none none none',
+            },
+        })
 
-            gsap.from(projectRightRef.current, {
-                translateX: '1000px',
-                duration: 1.1,
-                opacity: 0,
-                ease: 'power4.out',
-                scrollTrigger: {
-                    trigger: projectRightRef.current,
-                    start: 'top-=700px',
-                    end: 'bottom center',
-                    toggleActions: 'play none none none',
-                },
-            })
+        gsap.from(projectRightRef.current, {
+            translateX: '1000px',
+            duration: 1.1,
+            opacity: 0,
+            ease: 'power4.out',
+            scrollTrigger: {
+                trigger: projectRightRef.current,
+                start: 'top-=700px',
+                end: 'bottom center',
+                toggleActions: 'play none none none',
+            },
+        })
     }, [])
 
     return (
@@ -105,6 +106,21 @@ function Projects() {
                 <p>Hey there! Welcome to my little portfolio world, where I've poured heart and soul into this adorable projects. From cute designs to clever solutions, it's a snug spot with a lot of heart. This trio is just the start of the fun—more cozy projects coming your way soon!</p>
             </div>
             <div className="grid">
+                <div ref={projectRightRef} className="box">
+                    <div className="text">
+                        <div className="button"><p>2024-06</p></div>
+                        <p className="heading">Taskager</p>
+                        <p className='par'>This is one of my biggest Projects . The Stack is React and sass frontend , NestJS backend and i used mongoDB database . But because of deployment problems you can see it only as github repository.</p>
+                        <div onClick={() => {
+                            window.location.href = 'https://github.com/elizbaryananush/TaskSystem2'
+                        }} className="button"> <p>View</p> <span>
+                                <svg xmlns="http://www.w3.org/2000/svg">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M3.88153 12.6302C3.88153 12.3776 3.97758 12.1249 4.17035 11.9322L8.73614 7.36703L4.17035 2.80189C3.78548 2.41637 3.78548 1.79137 4.17035 1.40584C4.55587 1.02097 5.18087 1.02097 5.5664 1.40584L10.8296 6.669C11.2144 7.05453 11.2144 7.67953 10.8296 8.06506L5.5664 13.3282C5.18087 13.7131 4.55587 13.7131 4.17035 13.3282C3.97758 13.1355 3.88153 12.8828 3.88153 12.6302Z" />
+                                </svg>
+                            </span></div>
+                    </div>
+                    <img src={taskager} alt="" />
+                </div>
                 <div ref={projectLeftRef1} className="box">
                     <img src={yourbank} alt="" />
                     <div className="text">
